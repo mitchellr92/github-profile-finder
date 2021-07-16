e2be8d87cee2f01cf4ef3cbfba202819c8fce920
Mitchell Robles <mitchellr92@live.com>
2021-07-16 11:50:27 -0700

    created temporary array for github users, displayed users in browser

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-import React, { Component } from 'react';
-import Navbar from './components/layout/Navbar';
-import UserItem from "./components/users/UserItem";
-import './App.css';
+import React, { Component } from "react";
+import Navbar from "./components/layout/Navbar";
+import Users from "./components/users/Users";
+import "./App.css";
 
 class App extends Component {
-
   render() {
-
     return (
       <div className="App">
         <Navbar />
-        <UserItem />
+        <div className="container">
+          <Users />
+        </div>
       </div>
     );
   }
-
 }
 
 export default App;
