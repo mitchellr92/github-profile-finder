ad3653a4d9398ecc7a93bdefb0d0c0390cfc6493
Mitchell Robles <mitchellr92@live.com>
2021-08-23 11:10:23 -0700

    Set up context for alert

--- src/context/types.js
+++ src/context/types.js
@@ -4,4 +4,5 @@ export const CLEAR_USERS = "CLEAR_USERS";
 export const GET_REPOS = "GET_REPOS";
 export const SET_LOADING = "SET_LOADING";
 export const SET_USERS = "SET_USERS";
+export const SET_ALERT = "SET_ALERT"
 export const REMOVE_ALERT = "REMOVE_ALERT";
